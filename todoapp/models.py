e913be9ccebf1f315c8cf6a9cf368658269ca957
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-01-04 14:50:59 +0100

    created models and first view; linked with django project

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
+from django.db.models.fields import CharField, DateTimeField
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    pub_date = models.DateTimeField('date published')
