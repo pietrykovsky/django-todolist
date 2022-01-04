310d31f788a944204aad4b44ec94e0a177ee8d72
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-01-04 19:34:05 +0100

    created static files and added views

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.db.models.constraints import UniqueConstraint
-from django.db.models.fields import CharField, DateTimeField
 
 # Create your models here.
 class User(models.Model):
@@ -8,8 +6,14 @@ class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.username
+
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world - index")
+def home(request):
+    template = loader.get_template('todoapp/home.html')
+    return HttpResponse(template.render({},request))
+
+def about(request):
+    template = loader.get_template('todoapp/about.html')
+    return HttpResponse(template.render({},request))
+
+def contact(request):
+    template = loader.get_template('todoapp/contact.html')
+    return HttpResponse(template.render({},request))
