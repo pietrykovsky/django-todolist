dac126c6f0ba1587255bf79751e1b3a80753f71e
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-01-10 23:09:30 +0100

    implemented task CRUD system and user authentication system

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from . import views
+from .views import about, contact, HomeView, TaskEditView, TaskDeleteView, TaskDetailView, LoginView, RegisterView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('about/', views.about, name= 'about'),
-    path('contact/', views.contact, name= 'contact'),
-    path('', views.TaskListView.as_view(), name='home'),
-    path('create-task/', views.TaskCreateView.as_view(), name='create-task'),
-    path('edit-task/<int:pk>/', views.TaskEditView.as_view(), name='edit-task')
+    path('about/', about, name= 'about'),
+    path('contact/', contact, name= 'contact'),
+    path('', HomeView.as_view(), name='home'),
+    path('edit/<int:pk>/', TaskEditView.as_view(), name='task-edit'),
+    path('delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
+    path('detail/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
     ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,39 +1,86 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from .forms import UserForm, TaskForm
-from django.views.generic import ListView
-from .models import Task
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 
+from django.views import View
+from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView, DeleteView, FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from .models import Task
+from .forms import UserForm, TaskForm
 
 # Create your views here.
-def about(request):
-    template = loader.get_template('todoapp/about.html')
-    return HttpResponse(template.render({},request))
-
-def contact(request):
-    template = loader.get_template('todoapp/contact.html')
-    return HttpResponse(template.render({},request))
-
-class TaskListView(ListView):
-    model = Task
+class HomeView(View):
     template_name = 'todoapp/home.html'
-    context_object_name = 'tasks'
+    http_methods_names = ['get', 'post']
+    def get(self, request):
+        form = TaskForm()
+        if request.user.is_authenticated:
+            tasks = Task.objects.filter(user=request.user)
+        else:
+            tasks = None
+        context = {'form': form, 'tasks': tasks}
+        return render(request, self.template_name, context)
 
-class TaskCreateView(CreateView):
+    @login_required
+    def post(self, request):
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+
+        return redirect(reverse('home'))
+
+class TaskDetailView(LoginRequiredMixin ,DetailView):
     model = Task
-    template_name = 'todoapp/task-form.html'
+    context_object_name = 'task'
+    template_name = 'todoapp/task-detail.html'
+
+class TaskEditView(LoginRequiredMixin ,UpdateView):
+    model = Task
     fields = ['title']
-    context_obect_name = 'tasks'
+    context_object_name = 'form'
+    template_name = 'todoapp/task-edit.html'
     success_url = reverse_lazy('home')
 
-class TaskEditView(UpdateView):
+class TaskDeleteView(LoginRequiredMixin ,DeleteView):
     model = Task
-    template_name = 'todoapp/task-form.html'
-    fields = ['title']
-    context_obect_name = 'tasks'
     success_url = reverse_lazy('home')
 
-class TaskDelete(DeleteView):
-    pass
+    def get(self, request):
+        return self.post(request)
+    
+# add informations about the author
+def about(request):
+    return render(request, 'todoapp/about.html')
+
+# add sending mail functionality
+def contact(request):
+    return render(request, 'todoapp/contact.html')
+
+class LoginView(LoginView):
+    template_name = 'todoapp/login.html'
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+class RegisterView(FormView):
+    template_name = 'todoapp/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
+    
